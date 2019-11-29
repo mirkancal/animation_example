9cb16915e65227e58194f2be7df34644df6d3540
Mirkan <mirkancaliskan@gmail.com>
2019-11-29 21:39:10 +0300

    Complete addListener

--- lib/main.dart
+++ lib/main.dart
@@ -1,20 +1,37 @@
 import 'package:flutter/material.dart';
 
-void main() => runApp(MyApp());
+void main() => runApp(LogoApp());
 
-class MyApp extends StatelessWidget {
+class LogoApp extends StatefulWidget {
+  @override
+  _LogoAppState createState() => _LogoAppState();
+}
+
+class _LogoAppState extends State<LogoApp> with SingleTickerProviderStateMixin {
+  Animation animation;
+  AnimationController animationController;
+  @override
+  void initState() {
+    animationController =
+        AnimationController(vsync: this, duration: Duration(seconds: 1));
+    animation =
+        Tween<double>(begin: 200.0, end: 400.0).animate(animationController);
+    animationController.addListener(() {
+      setState(() {});
+    });
+
+    animationController.forward();
+    super.initState();
+  }
+
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Material App',
-      home: Scaffold(
-        appBar: AppBar(
-          title: Text('Material App Bar'),
-        ),
-        body: Center(
-          child: Container(
-            child: Text('Hello World'),
-          ),
+    return Center(
+      child: Container(
+        height: animation.value,
+        width: animation.value,
+        child: FlutterLogo(
+          colors: Colors.red,
         ),
       ),
     );
