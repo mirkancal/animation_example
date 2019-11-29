325aa74fd082db2069ad8fbc0563fd3f365fea7e
Mirkan <mirkancaliskan@gmail.com>
2019-11-29 21:54:29 +0300

    Add two tweens example

--- lib/main.dart
+++ lib/main.dart
@@ -15,7 +15,7 @@ class _LogoAppState extends State<LogoApp> with SingleTickerProviderStateMixin {
     animationController =
         AnimationController(vsync: this, duration: Duration(seconds: 1));
     animation =
-        Tween<double>(begin: 200.0, end: 400.0).animate(animationController);
+        CurvedAnimation(parent: animationController, curve: Curves.easeIn);
 
     animationController.addStatusListener((status) {
       if (status == AnimationStatus.dismissed) {
@@ -33,31 +33,49 @@ class _LogoAppState extends State<LogoApp> with SingleTickerProviderStateMixin {
   @override
   Widget build(BuildContext context) {
     return Center(
-      child: HeartbeatEffect(
-        animation: animation,
+      child: AnimatedLogo(animation: animation),
+    );
+  }
+}
+
+class AnimatedLogo extends AnimatedWidget {
+  AnimatedLogo({this.animation}) : super(listenable: animation);
+
+  static final _opacityTween = Tween<double>(begin: 0.0, end: 1.0);
+  static final _sizeTween = Tween<double>(begin: 200.0, end: 400.0);
+
+  final Animation animation;
+
+  @override
+  Widget build(BuildContext context) {
+    return Opacity(
+      opacity: _opacityTween.evaluate(animation),
+      child: Container(
+        height: _sizeTween.evaluate(animation),
+        width: _sizeTween.evaluate(animation),
         child: FlutterLogo(),
       ),
     );
   }
 }
 
-class HeartbeatEffect extends StatelessWidget {
-  HeartbeatEffect({this.animation, this.child});
-  final Animation animation;
-  final Widget child;
-  @override
-  Widget build(BuildContext context) {
-    return AnimatedBuilder(
-      animation: animation,
-      child: child,
-      builder: (context, child) => Container(
-        height: animation.value,
-        width: animation.value,
-        child: child,
-      ),
-    );
-  }
-}
+// class HeartbeatEffect extends StatelessWidget {
+//   HeartbeatEffect({this.animation, this.child});
+//   final Animation animation;
+//   final Widget child;
+//   @override
+//   Widget build(BuildContext context) {
+//     return AnimatedBuilder(
+//       animation: animation,
+//       child: child,
+//       builder: (context, child) => Container(
+//         height: animation.value,
+//         width: animation.value,
+//         child: child,
+//       ),
+//     );
+//   }
+// }
 
 // class AnimatedLogo extends AnimatedWidget {
 //   const AnimatedLogo({
